82e15de89ee2840b005610e2ad3f81f034cf574d
Nguyễn Huy Hoà <81904865+hoanguyen6611@users.noreply.github.com>
2022-04-27 13:49:19 +0700

    tu2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './App.css';
-import Blog from './components/Blog';
+import Dropdown from './components/Dropdown';
+import SidebarMenu from './components/SidebarMenu';
+import useClickOutSide from './hooks/useClickOutSide';
 
 function App() {
-  
+  const {
+    show,
+    setShow,
+    nodeRef
+  } = useClickOutSide("button");
   return (
     <div className="p-5">
-      <Blog></Blog>
+      <button className="inline-block m-3 p-3 rounded-lg text-white bg-green-400"
+        onClick={() => setShow(true)}
+      >Show menu</button>
+      <SidebarMenu show={show} ref={nodeRef}></SidebarMenu>
+      <Dropdown></Dropdown>
     </div>
   );
 }
