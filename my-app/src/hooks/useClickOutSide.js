596c81ae2effa4c61db58b86053c644c47ee86a3
Nguyễn Huy Hoà <81904865+hoanguyen6611@users.noreply.github.com>
2022-04-27 13:41:21 +0700

    sidebarmenu with hook

--- my-app/src/hooks/useClickOutSide.js
+++ my-app/src/hooks/useClickOutSide.js
@@ -1,17 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 
-export default function useClickOutSide(){
+export default function useClickOutSide(dom="button"){
     const [show, setShow] = useState(false);
     const nodeRef = useRef(null);
     useEffect(() =>{
-        function dropdownClickOutDropdown(e){
-            if (nodeRef.current && !nodeRef.current.contains(e.target)){
+        function dropdownClickOutSide(e){
+            if (nodeRef.current && !nodeRef.current.contains(e.target)&&!e.target.matches(dom)){
                 setShow(false);
             }
         }
-        document.addEventListener("click",dropdownClickOutDropdown);
+        document.addEventListener("click",dropdownClickOutSide);
         return ()=>{
-            document.removeEventListener("click",dropdownClickOutDropdown);
+            document.removeEventListener("click",dropdownClickOutSide);
         }
     },[]);
     return {
