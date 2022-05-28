486d56588b0ad89b76e4ab80424aaf515cd607f6
Nguyễn Huy Hoà <81904865+hoanguyen6611@users.noreply.github.com>
2022-05-28 11:05:30 +0700

    Portal and Modal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
-import Input from './components/form/Input';
+import { useState } from "react";
+import "./App.css";
+import Modal from './components/modal/Modal';
+// import DropdownPortal from "./components/other/DropdownPortal";
+// import Tooltip from "./components/other/Tooltip";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div>
-      <Input></Input>
-    </div>
+    <>
+      <div>
+        <Modal open={showModal} handleClose={() => setShowModal(false)}></Modal>
+      </div>
+      <button
+        className="p-4 text-white bg-blue-500 rounded-md m-5"
+        onClick={() => setShowModal(true)}
+      >
+        Show modal
+      </button>
+      <div className="relative z-30 m-5">
+        Lorem ipsum dolor sit amet, consectetur adip
+      </div>
+      {/* <div className="overflow-hidden m-5">
+      <DropdownPortal></DropdownPortal>
+      </div> */}
+      {/* <div className="m-10">
+        <Tooltip></Tooltip>
+      </div> */}
+    </>
   );
 }
 
--- my-app/src/components/other/Dropdown.js
+++ my-app/src/components/other/Dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useClickOutSide from '../hooks/useClickOutSide';
+import useClickOutSide from './../../hooks/useClickOutSide';
 
 const Dropdown = () => {
     const {
